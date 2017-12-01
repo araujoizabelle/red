56bbaaa0c7da9344c8ade488e6515c9851991e74
araujoizabelle <izabellederrihu64@gmail.com>
2017-12-01 18:10:31 -0200

    meu DEUS

---
+++ erro.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<link rel="stylesheet" href="./css/erro.css"/>
+	<meta charset="utf-8">
+</head>
+<body> 
+<h2 class="mensagem-erro"> <?php
+echo "Erro: ", $_GET['erro'];
+?> 
+
+</body>
+
+</html>
